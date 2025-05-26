ef54e045d33c3c9f66341c72d6e2597a674ba342
Ana Carolina <123709099+acmachado48@users.noreply.github.com>
2025-05-26 20:30:37 -0300

    firebase teste

--- Projeto Final/flutter_application_final/lib/main.dart
+++ Projeto Final/flutter_application_final/lib/main.dart
@@ -1,7 +1,11 @@
 import 'package:flutter/material.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'firebase_options.dart';
 import 'login.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
   runApp(const MyApp());
 }
 
@@ -13,7 +17,7 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'FitLab App',
       theme: ThemeData(primarySwatch: Colors.blue),
-      home: const Login(), //Chama a tela de login
+      home: const Login(), // Chama a tela de login
       debugShowCheckedModeBanner: false,
     );
   }
